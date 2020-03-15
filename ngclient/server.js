68538c683f1ede13a7380395b5af4d7db3ddb259
Tobias Koller <tobias.koller@students.fhnw.ch>
2020-03-15 15:55:15 +0100

    proxy for api

--- ngclient/server.js
+++ ngclient/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const options = {
+    target: 'http://127.0.0.1:5000', // target host
+    // pathRewrite: {
+    //   '^/api/old-path': '/api/new-path', // rewrite path
+    //   '^/api/remove/path': '/path' // remove base path
+    // }
+  };
+  
 
 app.use(express.static(__dirname + '/dist/oshop'));
+
+app.use('/api', createProxyMiddleware(options));
 
 //
 
@@ -9,4 +21,5 @@ app.get('/*', (req,res) => {
     res.sendFile(__dirname + '/dist/oshop/index.html');
 });
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+app.listen(4200);
